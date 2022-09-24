968960ce1eba431808a2812b3068c566a23fd4b6
Igoryok8 <energymag@mail.ru>
2022-09-25 02:32:39 +0300

    Задача 1, 2 и 3 выполнены

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,34 @@ public class Main {
         System.out.println ("Значение переменной cake с типом float равно " + cake );
         double game = 33.88888888;
         System.out.println ("Значение переменной game с типом double равно " + game );
+        System.out.println ("Задача 2 ");
+        int a = 27897;
+        System.out.println (a);
+        byte b = 2;
+        System.out.println (b);
+        short c = 786;
+        System.out.println (c);
+        long d = 987678965549L;
+        System.out.println (d);
+        float e = 27.12f;
+        System.out.println (e);
+        double f = 67.0;
+        System.out.println (f );
+        System.out.println ("Задача 3 ");
+        byte classRoom1 = 23;
+        System.out.println ("У Людмилы Павловны " +classRoom1+ " ученика");
+        byte classRoom2 = 27;
+        System.out.println ("У Анны Сергеевны " +classRoom2+ " учеников");
+        byte classRoom3 = 30;
+        System.out.println ("У Екатерины Андреевны " +classRoom3+ " учеников");
+        short paper = 480;
+        System.out.println ("Три учительницы закупили все вместе " +paper+ " листов бумаги на все три класса. ");
+        int totalStudents = classRoom1 + classRoom2 + classRoom3;
+        System.out.println ("Общее количество учеников в трёх классах: " + totalStudents);
+        int oneStudentPaper = paper / totalStudents;
+        System.out.println ("На каждого ученика рассчитано " +oneStudentPaper+ " листов бумаги.");
+
+
 
     }
 }
