58b15ad85423830753a7953ae39f16192ea99b84
Igoryok8 <energymag@mail.ru>
2022-09-25 01:30:16 +0300

    Homework 4

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println ("Задача 1 ");
+        int ball = 50;
+        System.out.println ("Значение переменной ball с типом int равно " + ball );
+        byte book = 100;
+        System.out.println ("Значение переменной book с типом byte равно " + book );
+        short stadium = 1000;
+        System.out.println ("Значение переменной stadium с типом short равно " + stadium );
+        long team = 888L;
+        System.out.println ("Значение переменной team с типом long равно " + team );
+        float cake = 3.5f;
+        System.out.println ("Значение переменной cake с типом float равно " + cake );
+        double game = 33.88888888;
+        System.out.println ("Значение переменной game с типом double равно " + game );
+
+    }
+}
